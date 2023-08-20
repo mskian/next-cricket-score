a5e79c5f2128f59d9e1dddaaa96eb4b21ea8d427
Santhosh Veer <veerfun23@gmail.com>
2023-08-20 19:19:32 +0530

    Update route.js

--- app/live/route.js
+++ app/live/route.js
@@ -36,7 +36,7 @@ const dummy_json = {
 
 export async function GET() {
   try {
-    const str = "https://m.cricbuzz.com/live-cricket-scores/77242"
+    const str = "https://m.cricbuzz.com/live-cricket-scores/77249"
     //const live_url = str.replace('www', 'm');
     const response = await fetch(str, {
       headers: {
